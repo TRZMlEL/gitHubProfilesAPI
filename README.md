09315e6282e2d2b60227ad04c66e890ddcfb880a
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-11-12 22:58:04 +0100

    add ReadMe file

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# Vue 3 + Vite
+# GitHub Profiles
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+## Description
+Using the GitHub API, I created a page to check users of this service. I drew inspiration for creating this page from the repository: [50projects50days](https://github.com/bradtraversy/50projects50days "50projects50days")
 
-## Recommended IDE Setup
+## Screenshots
+### Desktop view
+![Desktop view 1](https://cdn.discordapp.com/attachments/783723569016340523/1173373985522716692/image.png?ex=6563b8ae&is=655143ae&hm=ecf4410dc5c2da4e4fe2e5b479edaeda2487b0f5586feb828b942afe0a1e87ee& "Desktop view 1")
 
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+![Desktop view 2](https://cdn.discordapp.com/attachments/783723569016340523/1173374287885897788/image.png?ex=6563b8f6&is=655143f6&hm=b917397e985a66f12f75eb00e4570f54a4a2071ba761f2e05c15f0f28cfcea31& "Desktop view 2")
+### Mobile view
+![Mobile view 1](https://cdn.discordapp.com/attachments/783723569016340523/1173374417649278996/image.png?ex=6563b915&is=65514415&hm=0b040fa5af675778668ad5b707122747fee7b35ce85480a6552898d53fc1a236& "Mobile version 1")
+
+![Mobile view 1](https://cdn.discordapp.com/attachments/783723569016340523/1173374470921142352/image.png?ex=6563b922&is=65514422&hm=99a7ad3ea460c83f087952855cc643c42048d7bd367e9e0f5c40fd56a5f68029& "Mobile view 1")
+
+## How to run the code
+To run the code, you must have node.js installed.
+Then in the terminal in the project folder, you need to type 
+
+ `npm install `
+ 
+and then 
+
+`npm run dev`
+
+Then click on the localhost link.
+
+## Used tools
+
+- Figma
+- JavaScript
+- Vite
+- Vue 3
+- Tailwindcss
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <body class="bg-gray-950 flex items-center justify-center w-screen h-screen text-light bg-gradient-to-br from-gradient1 via-gradient2 to-gradient3 animate-gradient flex-col p-10">
-    <div class="flex items-center justify-center w-11/12 md:w-3/5 lg:w-1/3">
+    <header class="flex items-center justify-center w-11/12 md:w-3/5 lg:w-1/3">
       <input type="text" v-model="username" @keyup.enter="getData()" placeholder="GitHub username" autofocus class="bg-dark h-2/12 w-full rounded-xl p-4">
       <button @click="getData()" class="w-1/12 aspect-square text-5xl flex items-center justify-center m-4">🔍︎</button>
-    </div>
-    <div v-if="userData" class=" w-11/12 md:w-3/4 lg:w-1/2 bg-dark flex flex-col p-5 rounded-2xl">
+    </header>
+    <main v-if="userData" class=" w-11/12 md:w-3/4 lg:w-1/2 bg-dark flex flex-col p-5 rounded-2xl">
       <div class="flex flex-col md:flex-row">
         <img :src="userData.avatar_url" class="rounded-full w-1/4 aspect-square mr-5 mb-5  "/>
         <div class="flex flex-col gap-2.5">
@@ -20,20 +20,21 @@
       <div class="flex flex-wrap gap-5 mt-5">
         <div v-for="repo in reposData && reposData.slice(0, 5)" class="bg-color p-2 rounded-lg"><a :href="repo.html_url"> {{ repo.name }} </a></div>
       </div>
-    </div>
+    </main>
   </body>
 </template>
 
 <script setup>
   import { ref } from 'vue';
 
-  const APIURL = 'https://api.github.com/users/'
+  const APIURL = 'https://api.github.com/users/' // GitHub API
   let username = "", userData = ref(), reposData = ref();
 
+  // Function to fetch user data and their repositories
   const getData = async () => {
-    const response1 = await fetch(APIURL + username);                         
-    userData.value =  await response1.json();
-    const response2 = await fetch(APIURL + username + '/repos?sort=created'); 
-    reposData.value = await response2.json();
+     // Fetching user data
+    const response1 = await fetch(APIURL + username); userData.value =  await response1.json();
+    // Fetching user repositories
+    const response2 = await fetch(APIURL + username + '/repos?sort=created'); reposData.value = await response2.json();
   }
 </script>
