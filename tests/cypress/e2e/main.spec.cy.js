0157cf3b8f5baf34c33a092c94b62d3fe37c031e
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-11-15 21:08:04 +0100

    test input API and link

--- tests/cypress/e2e/main.spec.cy.js
+++ tests/cypress/e2e/main.spec.cy.js
@@ -1,5 +1,12 @@
 describe('mainTest', () => {
-  it('User input and API data retrieval validation', () => {
-    cy.visit('http://localhost:5173')
+  it('User input and API data retrieval validation. Do all repos have a link?', () => {
+    cy.visit('http://localhost:5173');
+
+    cy.get('input').type("trzmlel");
+    cy.get('button').click();
+
+    cy.get('.bg-color').each(($el, index, $list) => {
+      cy.wrap($el).find('a').should('have.attr', 'href')
+    })
   })
 })
